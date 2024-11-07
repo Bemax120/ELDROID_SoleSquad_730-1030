dc7b00793d4f27e3341066080248b7777712a29c
Mikaayyy <ruffamendoza5@gmail.com>
2024-11-08 03:17:08 +0800

    Ruffa Mae Mendoza - Register UI

---
+++ app/src/main/java/com/eldroid/solemate/RegisterActivity.kt
@@ -0,0 +1,14 @@
+package com.eldroid.solemate
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_register)
+
+    }
+}
